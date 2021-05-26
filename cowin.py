df4b870b9f9c380fdf4503b90e74d41ee7de17a7
Amit kumar Yadav <Amit.Yadav@go-mmt.com>
2021-05-26 12:07:08 +0530

    Refractored code to remove duplications DRY

--- cowin.py
+++ cowin.py
@@ -16,7 +16,7 @@ class SearchType(Enum):
 
 
 district_id_chat_ids_map = {
-    651: [],
+    651: [], # pass telegram chat ids here
 }
 
 pincode_chat_ids_map = {
@@ -25,61 +25,47 @@ pincode_chat_ids_map = {
 }
 
 
-def get_cowin_response_by_district(district_id, date):
+def get_cowin_response(search_type, code, date):
     headers = {
         'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
     }
-    url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=%s&date=%s" % (
-        district_id, date)
+
+    if search_type == SearchType.PINCODE:
+        url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=%s&date=%s"
+    else:
+        url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=%s&date=%s"
+
+    url = url % (code, date)
     r = requests.get(
         url,
         headers=headers
     )
+
+    # Not doing this as I want the error to be printed in infinite while loop
+    # if r.status_code != 200:
+    #     return None
 
     res = r.text
     parsed_res = json.loads(res, object_hook=lambda d: SimpleNamespace(**d))
     return parsed_res
 
 
-def get_cowin_response_by_pincode(pincode, date):
-    headers = {
-        'User-Agent':
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
-    }
-    url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=%s&date=%s" % (
-        pincode, date)
-    r = requests.get(
-        url,
-        headers=headers
-    )
-
-    res = r.text
-    parsed_res = json.loads(res, object_hook=lambda d: SimpleNamespace(**d))
-    return parsed_res
-
-
-def check_dose1_and_get_centers(pincode=None, district_id=None):
-    available_centers = []
-
-    if not any([pincode, district_id]):
-        return available_centers
-
-    if all([pincode, district_id]):
-        raise Exception("Only one of pincode or district_id exptected")
-
+def check_dose1_and_get_centers(search_type, code):
     today = datetime.today().strftime("%d-%m-%Y")
 
     try:
-        if pincode:
-            resp = get_cowin_response_by_pincode(pincode=pincode, date=today)
-        else:
-            resp = get_cowin_response_by_district(district_id=district_id, date=today)
-    except Exception as e:
-        print("Exception occurred")
+        resp = get_cowin_response(search_type, code, date=today)
+    except Exception:
+        print("\tException occurred")
         print(traceback.format_exc())
         resp = None
 
+    return adapt_cowin_response(resp)
+
+
+def adapt_cowin_response(resp):
+    available_centers = []
     if not resp:
         return available_centers
 
@@ -95,11 +81,10 @@ def check_dose1_and_get_centers(pincode=None, district_id=None):
         if len(date_list) > 0:
             center_dict[center_name] = date_list
             available_centers.append(center_dict)
-
     return available_centers
 
 
-def get_formatted_response(available_centers):
+def format_adapted_response_for_telegram(available_centers):
     resp = ""
     for center in available_centers:
         for k in center:
@@ -115,47 +100,16 @@ def get_formatted_response(available_centers):
     return resp
 
 
-def run(wait_duration=60):
-    do_loop = True
-    pincode_formatted_response = None
-    district_formatted_response = None
-    while do_loop:
-        # TODO Make the below calls parallel
-        try:
-            pincode_formatted_response = hit_api_and_telegram_slots(
-                SearchType.PINCODE, pincode_chat_ids_map,
-                pincode_formatted_response, wait_duration
-            )
-            district_formatted_response = hit_api_and_telegram_slots(
-                SearchType.DISTRICT, district_id_chat_ids_map,
-                district_formatted_response, wait_duration
-            )
-        except Exception as e:
-            error = None
-            if hasattr(e, 'message'):
-                error = e.message
-            msg = "Code kahin fata h, dekh use jaldi se. %s" % error
-            users = []
-            telegram_msg(msg, users)
-        print("Sleeping for %s seconds" % wait_duration)
-        print("=" * 100)
-        # do_loop = False
-        time.sleep(wait_duration)
-
-
-def hit_api_and_telegram_slots(search_type, chat_map, formatted_response, wait_duration):
+def hit_cowin_api(search_type, chat_map, formatted_response, wait_duration):
     for k in chat_map:
         code = k
         registered_users = chat_map[k]
         print("\nChecking slots for %s: %s after %s seconds" % (search_type, code, wait_duration))
-        if search_type == SearchType.PINCODE:
-            available_centers = check_dose1_and_get_centers(pincode=str(code))
-        else:
-            available_centers = check_dose1_and_get_centers(district_id=str(code))
+        available_centers = check_dose1_and_get_centers(search_type, str(code))
         if len(available_centers) > 0:
             print("\tSending message on telegram")
             md5_hash_old = generate_hash(formatted_response)
-            formatted_response = get_formatted_response(available_centers)
+            formatted_response = format_adapted_response_for_telegram(available_centers)
             md5_hash_new = generate_hash(formatted_response)
             if md5_hash_old != md5_hash_new:
                 telegram_msg(formatted_response, registered_users)
@@ -181,6 +135,37 @@ def generate_hash(msg):
     return md5_hash
 
 
+def run(wait_duration_in_sec=60):
+    do_loop = True
+    pincode_formatted_response = None
+    district_formatted_response = None
+    while do_loop:
+        # TODO Make the below calls parallel
+        try:
+            pincode_formatted_response = hit_cowin_api(
+                SearchType.PINCODE, pincode_chat_ids_map,
+                pincode_formatted_response, wait_duration_in_sec
+            )
+            district_formatted_response = hit_cowin_api(
+                SearchType.DISTRICT, district_id_chat_ids_map,
+                district_formatted_response, wait_duration_in_sec
+            )
+        except Exception as e:
+            error = None
+            if hasattr(e, 'message'):
+                error = e.message
+            msg = "Code kahin fata h, dekh use jaldi se. %s" % error
+            users = []
+            telegram_msg(msg, users)
+        print("Sleeping for %s seconds" % wait_duration_in_sec)
+        print("=" * 100)
+        # do_loop = False
+        time.sleep(wait_duration_in_sec)
+
+
 if __name__ == "__main__":
-    run(30)
+    telegram_token = os.environ["telegram_bot_token"]
+    if not telegram_token:
+        raise Exception("No telegram token found")
+    run(wait_duration_in_sec=30)
 
